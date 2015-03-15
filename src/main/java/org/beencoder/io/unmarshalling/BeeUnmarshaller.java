24849d3cd171c0a82b352916a472d7627e4b5846
sergey.tityenok@logicify.com <tityenok@gmail.com>
2015-03-15 22:01:45 +0200

    several comments

--- src/main/java/org/beencoder/io/unmarshalling/BeeUnmarshaller.java
+++ src/main/java/org/beencoder/io/unmarshalling/BeeUnmarshaller.java
@@ -5,6 +5,12 @@ import org.beencoder.io.parsing.BeeElementsParser;
 import org.beencoder.type.element.BeeElement;
 
 /**
+ * Unmarshalls only 1 object at one call. If you need to get sequence of objects from a
+ * stream you can use {@link org.beencoder.io.parsing.BeeElementsParser} to get objects
+ * one by one.
+ * This can parse complex objects which are inside one objects e.g d1:ai5e1:bli1e2:bai3ee1:dd5:hello5:worldee
+ * but will not parse i1ei2e because it is not wrapped in one object.
+ *
  * Created by tityenok on 3/15/15.
  */
 public class BeeUnmarshaller
